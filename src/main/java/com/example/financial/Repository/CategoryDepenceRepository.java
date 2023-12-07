4b945642782161ac5893b3589826c0e9234e0870
mohamedderbali <mohamed.derbali@esprit.tn>
2023-12-07 23:32:40 +0100

    fc

--- src/main/java/com/example/financial/Repository/CategoryDepenceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.financial.Repository;
-
-import com.example.financial.Model.CategoryDepence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryDepenceRepository extends JpaRepository<CategoryDepence,Long> {
-}
--- src/main/java/com/example/financial/Repository/DepenceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.financial.Repository;
-
-import com.example.financial.Model.CategoryDepence;
-import com.example.financial.Model.Depence;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  DepenceRepository extends JpaRepository<Depence,Long> {
-}
